a7d1f10ee8a568b2a307539f4674ad0f64ac20d2
Richard Cho <rich4rdyc@gmail.com>
2022-06-05 13:45:43 -0700

    richard: updated test vector generator

--- WESAD data processing/test_vectors.m
+++ WESAD data processing/test_vectors.m
@@ -12,7 +12,7 @@ h_s = dec2bin(8.*fixdata(hrate(k)),11);
 t_s = dec2bin(8.*fixdata(in.signal.chest.Temp(k2)),9);
 e_s = dec2bin(8.*fixdata(in.signal.chest.EDA(k2)),8);
 
-fileID = fopen('stressed.txt','w');
+fileID = fopen('stressed_s17.txt','w');
 
 for n=1:size(h_s)
     fprintf(fileID,'%s\n',t_s(n,:));
@@ -27,7 +27,7 @@ h_ns = dec2bin(8.*fixdata(hrate(kns)),11);
 t_ns = dec2bin(8.*fixdata(in.signal.chest.Temp(k2ns)),9);
 e_ns = dec2bin(8.*fixdata(in.signal.chest.EDA(k2ns)),8);
 
-fileID = fopen('not_stressed.txt','w');
+fileID = fopen('not_stressed.txt_s17','w');
 
 for n=1:size(h_ns)
     fprintf(fileID,'%s\n',t_ns(n,:));
